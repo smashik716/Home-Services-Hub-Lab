f96f7640cc0a6cd30d3828327bbeb356feb00852
sommaas <21201111@uap-bd.edu>
2024-11-12 16:48:48 +0600

    Views initialized

--- homeservicemanagement/views.py
+++ homeservicemanagement/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import *
+from django.contrib.auth import authenticate, login, logout
+import datetime
+from django.core.exceptions import ObjectDoesNotExist
+
+
+
 
 # Create your views here.
+def notification():
+    status = Status.objects.get(status='pending')
+    new = Service_Man.objects.filter(status=status)
+    count = 0
+    for i in new:
+        count += 1
+    d = {'count': count, 'new': new}
+    return d
+
+
+def Home(request):
+    user = ""
+    error = ""
+    try:
+        user = User.objects.get(id=request.user.id)
+        try:
+            sign = Customer.objects.get(user=user)
+            error = "pat"
+        except:
+            pass
+    except:
+        pass
+    ser1 = Service_Man.objects.all()
+    ser = Service_Category.objects.all()
+    for i in ser:
+        count = 0
+        for j in ser1:
+            if i.category == j.service_name:
+                count += 1
+        i.total = count
+        i.save()
+    d = {'error': error, 'ser': ser}
+    return render(request, 'home.html', d)
