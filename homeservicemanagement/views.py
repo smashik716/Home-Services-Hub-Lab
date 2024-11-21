f452364f6fd6939c0591e2efa10b0793376d44b8
ShuvroScripter <shuvrobarua111@gmail.com>
2024-11-21 23:36:08 +0600

    update

--- homeservicemanagement/views.py
+++ homeservicemanagement/views.py
@@ -1,60 +1,8 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.models import User
-from .models import *
-from django.contrib.auth import authenticate, login, logout
-import datetime
-from django.core.exceptions import ObjectDoesNotExist
 
 
 
 
-# Create your views here.
-def notification():
-    status = Status.objects.get(status='pending')
-    new = Service_Man.objects.filter(status=status)
-    count = 0
-    for i in new:
-        count += 1
-    d = {'count': count, 'new': new}
-    return d
 
-
-def Home(request):
-    user = ""
-    error = ""
-    try:
-        user = User.objects.get(id=request.user.id)
-        try:
-            sign = Customer.objects.get(user=user)
-            error = "pat"
-        except:
-            pass
-    except:
-        pass
-    ser1 = Service_Man.objects.all()
-    ser = Service_Category.objects.all()
-    for i in ser:
-        count = 0
-        for j in ser1:
-            if i.category == j.service_name:
-                count += 1
-        i.total = count
-        i.save()
-    d = {'error': error, 'ser': ser}
-    return render(request, 'home.html', d)
-
-
-def contact(request):
-    error = False
-    if request.method == "POST":
-        n = request.POST['name']
-        e = request.POST['email']
-        m = request.POST['message']
-        status = Status.objects.get(status="unread")
-        Contact.objects.create(status=status, name=n, email=e, message1=m)
-        error = True
-    d = {'error': error}
-    return render(request, 'contact.html', d)
 
 
 
@@ -153,5 +101,6 @@ def Service_home(request):
     else:
         if sign.status.status == "pending":
             terro = "message1"
-    d = {'error': error, 'terro': terro}
-    return render(request, 'service_home.html', d)
+    e = {'error': error, 'terro': terro}
+    return render(request, 'service_home.html', e)
+
